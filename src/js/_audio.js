0c29dcdf4f6cc5f01270844012b7c7d663a088bb
Sam Smith <smithsp1@gmail.com>
2021-11-18 16:51:32 -0600

    implemented basic waveforms

--- src/js/_audio.js
+++ src/js/_audio.js
@@ -1,46 +1,47 @@
 function AudioSetup() {
-  volSlider.addEventListener("input", changeVolume, false)
-  mainGainNode = audioContext.createGain()
-  mainGainNode.connect(audioContext.destination)
-  mainGainNode.gain.value = volSlider.value
+  volSlider.addEventListener("input", changeVolume, false);
+  mainGainNode = audioContext.createGain();
+  mainGainNode.connect(audioContext.destination);
+  mainGainNode.gain.value = volSlider.value;
   volValue.innerHTML = Math.round(volSlider.value * 100);
+
+  waveSlider.addEventListener("input", changeWaveform, false);
 }
 
 function playWave(frequency, velocity) {
-  let osc = audioContext.createOscillator()
-  let velocityGainNode =audioContext.createGain()
-  velocityGainNode.connect(mainGainNode)
-  velocityGainNode.gain.value = velocity
-  osc.connect(velocityGainNode)
-  osc.type = "sine"
-  osc.frequency.value = frequency
-  osc.start()
-  return osc
+  var osc = audioContext.createOscillator();
+  var velocityGainNode =audioContext.createGain();
+  velocityGainNode.connect(mainGainNode);
+  velocityGainNode.gain.value = velocity;
+  osc.connect(velocityGainNode);
+  osc.type = waveformType;
+  osc.frequency.value = frequency;
+  osc.start();
+  console.log(osc);
+  return osc;
 }
 
 function onNoteOn(e) {
   console.log(e);
-  console.log(e.velocity)
-  let frequency = frequencyTable[e.note.octave][e.note.name]
-  console.log(frequency)
-  oscMap.set(e.note.name + e.note.octave, playWave(frequency, e.velocity))
-  console.log(oscMap)
+  var frequency = frequencyTable[e.note.octave][e.note.name];
+  oscMap.set(e.note.name + e.note.octave, playWave(frequency, e.velocity));
+  console.log(oscMap);
 }
 
 function onNoteOff(e) {
   console.log(e);
-  var osc = oscMap.get(e.note.name + e.note.octave)
+  var osc = oscMap.get(e.note.name + e.note.octave);
   osc.stop();
-  oscMap.delete(e.note.name + e.note.octave)
+  oscMap.delete(e.note.name + e.note.octave);
 }
 
 function onPitchBend(e) {
-  console.log(e)
+  console.log(e);
   //bend range in cents
-  let bendRange = 200
-  let bendValue = e.value
+  var bendRange = 200;
+  var bendValue = e.value;
 
-  for(let [key, value] of oscMap) {
-    value.detune.setValueAtTime(bendRange*bendValue, audioContext.currentTime)
+  for(var [key, value] of oscMap) {
+    value.detune.setValueAtTime(bendRange*bendValue, audioContext.currentTime);
   }
 }
--- src/js/_page.js
+++ src/js/_page.js
@@ -1,46 +1,69 @@
-let audioContext = new (window.AudioContext || window.webkitAudioContext)()
-let oscMap = new Map()
-let mainGainNode = null
-let volSlider = document.getElementById("volume-slider")
-let volValue = document.getElementById("volume-value")
-let frequencyTable = getNoteFreqTable()
-AudioSetup()
-MIDISetup()
+let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+let oscMap = new Map();
+let mainGainNode = null;
+let volSlider = document.getElementById("volume-slider");
+let volValue = document.getElementById("volume-value");
+let waveSlider = document.getElementById("wave-slider");
+let frequencyTable = getNoteFreqTable();
+var waveformType = "sine";
+AudioSetup();
+MIDISetup();
 
 function midiDeviceDropdown() {
-  var dropbtn = document.getElementById("dropdown-button-id")
-  var dropContent = document.getElementById("device-list")
-  console.log(dropbtn)
+  var dropbtn = document.getElementById("dropdown-button-id");
+  var dropContent = document.getElementById("device-list");
   if(dropbtn.classList.contains("button-open")) {
-    dropbtn.classList.remove("button-open")
-    dropContent.classList.remove("dropdown-open")
+    dropbtn.classList.remove("button-open");
+    dropContent.classList.remove("dropdown-open");
 
   } else {
-    dropbtn.classList.add("button-open")
-    dropContent.classList.add("dropdown-open")
+    dropbtn.classList.add("button-open");
+    dropContent.classList.add("dropdown-open");
   }
 }
 
 function addInputButton(name, index){
-  var buttonID = "input-device-"+index
-  var newButton = document.createElement("button")
-  newButton.id = buttonID
-  newButton.classList.add("midi-device-selector")
+  var buttonID = "input-device-"+index;
+  var newButton = document.createElement("button");
+  newButton.id = buttonID;
+  newButton.classList.add("midi-device-selector");
   newButton.addEventListener("click", function() {
-    var button = document.getElementById(buttonID)
+    var button = document.getElementById(buttonID);
     for(var el of document.getElementsByClassName("selected"))
     {
-      el.classList.remove("selected")
+      el.classList.remove("selected");
     }
-    button.classList.add("selected")
-    chooseInput(name)
-    document.getElementById("dropdown-button-id").innerHTML = name + ' <i class="dropdown-icon"></i>'
-  })
-  newButton.appendChild(document.createTextNode(name))
-  document.getElementById("device-list").appendChild(newButton)
+    button.classList.add("selected");
+    chooseInput(name);
+    document.getElementById("dropdown-button-id").innerHTML = name + ' <i class="dropdown-icon"></i>';
+  });
+  newButton.appendChild(document.createTextNode(name));
+  document.getElementById("device-list").appendChild(newButton);
 }
 
 function changeVolume() {
   mainGainNode.gain.value = volSlider.value;
   volValue.innerHTML = Math.round(this.value*100);
+}
+
+function changeWaveform() {
+  var waveformNumber = waveSlider.value;
+  switch(waveformNumber) {
+    case '0':
+      waveformType = "sine";
+      break;
+    case '1':
+      waveformType = "square";
+      break;
+    case '2':
+      waveformType = "sawtooth";
+      break;
+    case '3':
+      waveformType = "triangle";
+      break;
+    case '4':
+      //TODO: implement custom wave type
+      waveformType = "custom";
+      break;
+  }
 }
