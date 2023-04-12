17e644f1fe6d9f8e952f7c175caba7cc0059fba1
Sam Smith <smithsp1@gmail.com>
2023-04-12 11:41:34 -0500

    refactor init, moving to gulp and ts

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+//import { module } from ./module_filename
+function hello(compiler) {
+    console.log("Hello from ".concat(compiler));
+    console.log('test');
+}
+hello("TypeScript");
---
+++ src/ts/main.ts
@@ -0,0 +1,7 @@
+//import { module } from ./module_filename
+
+function hello(compiler: string) {
+  console.log(`Hello from ${compiler}`);
+  console.log('test');
+}
+hello("TypeScript");
