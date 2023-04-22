d55c6a6e6c16456db67842716beac89c1d37cd1b
Sam Smith <smithsp1@gmail.com>
2023-04-22 01:03:36 -0500

    Keyboard commentary

--- src/ts/Keyboard.ts
+++ src/ts/Keyboard.ts
@@ -1,5 +1,17 @@
 import { Synth } from './Synth'
 
+/*
+Keyboard Class
+
+This class handles all input from the user's (computer) keyboard, it has:
+A pressedKeys set, containing all keyboard keys that the user is currently pressing,
+a droneKeys set, a set containing keys that are sustained via drone AKA Caps Lock,
+a sustainKeys set, a set containing keys that are sustained via sustain input AKA Shift or Space,
+a bindingMap, a map holding the binding of keyboard keys to MIDI notes, TODO: add key mapping customization,
+a controlsKets set, containing the keyboard key codes that are used NOT used for playing notes AKA sustain and drone controls,
+a drone boolean variable, whether drone record is on,
+a sustain boolean variable, whether the sustain signal is active
+*/ 
 export class Keyboard {
     pressedKeys: Set<string> = new Set()
     droneKeys: Set<string> = new Set()
@@ -55,7 +67,6 @@ export class Keyboard {
             if(!this.pressedKeys.has(e.code)) {
                 this.pressedKeys.add(e.code)
                 
-                console.log(e.code)
                 //pressed key is a note key
                 if(this.bindingMap.has(e.code)) {
                     //console.log("note start")
@@ -85,18 +96,25 @@ export class Keyboard {
     }
 
     clearAllNotes(synth: Synth) {
+        console.log("Clearing all notes:")
+        console.log("Pressed Keys -")
         this.pressedKeys.forEach((key) => {
             if(this.bindingMap.has(key)) {
+                console.log(key)
                 synth.triggerNoteStop(this.bindingMap.get(key))
             }
         })
+        console.log("Sustained Keys -")
         this.sustainKeys.forEach((key) => {
             if(this.bindingMap.has(key)) {
+                console.log(key)
                 synth.triggerNoteStop(this.bindingMap.get(key))
             }
         })
+        console.log("Drone Keys -")
         this.droneKeys.forEach((key) => {
             if(this.bindingMap.has(key)) {
+                console.log(key)
                 synth.triggerNoteStop(this.bindingMap.get(key))
             }
         })
