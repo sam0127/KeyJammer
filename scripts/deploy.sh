b74c32c75faa991caf7b2152689f43d0c770c2d2
Sam Smith <smithsp1@gmail.com>
2025-02-02 20:42:50 -0600

    deployment updates

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 git checkout --orphan gh-pages
 
-npm run build
+npm run build-prod
+rollup dist/src/index.js --format iife --file dist/js/index.js
+rm -rf dist/src
+uglifyjs dist/js/index.js -o dist/js/index.js --compress
 
 git --work-tree dist add --all
-git --work-tree dist commit -m "gh-pages"
+git --work-tree dist commit -m "gh-pages deployment"
 git push origin HEAD:gh-pages --force
 rm -r dist
 git checkout -f main
--- src/ts/Keyboard.ts
+++ src/ts/Keyboard.ts
@@ -1,17 +1,5 @@
 import { InputController } from './InputController'
 
-/*
-Keyboard Class
-
-This class handles all input from the user's (computer) keyboard, it has:
-A pressedKeys set, containing all keyboard keys that the user is currently pressing,
-a droneKeys set, a set containing keys that are sustained via drone AKA Caps Lock,
-a sustainKeys set, a set containing keys that are sustained via sustain input AKA Shift or Space,
-a bindingMap, a map holding the binding of keyboard keys to MIDI notes, TODO: add key mapping customization,
-a controlsKets set, containing the keyboard key codes that are used NOT used for playing notes AKA sustain and drone controls,
-a drone boolean variable, whether drone record is on,
-a sustain boolean variable, whether the sustain signal is active
-*/ 
 export class Keyboard {
     pressedKeys: Set<string> = new Set()
     readonly bindingMap: Map<string, string>
